a0b0e91ebef9855403a71dd5bd0312563869ea1e
Esteban <16k.ram@gmail.com>
2023-06-28 23:34:11 +0200

    no message

---
+++ src/main/java/araque/porqueras/Gmao/repositories/PreventivosRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package araque.porqueras.Gmao.repositories;
+
+import araque.porqueras.Gmao.models.Preventivos;
+import java.util.ArrayList;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author char_
+ */
+@Repository
+public interface PreventivosRepository extends CrudRepository<Preventivos, Long> {
+
+    public abstract ArrayList<Preventivos> findByDescripcionEquipo(String descripcionEquipo);
+}
---
+++ src/test/java/araque/porqueras/Gmao/GmaoApplicationTests.java
@@ -0,0 +1,13 @@
+package araque.porqueras.Gmao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
